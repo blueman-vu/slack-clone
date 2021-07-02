6e673f191d0296a2dfae034aba95f6560e640e01
blueman <blueman.vu@gmail.com>
2021-07-02 10:29:26 +0700

    basic setup app

---
+++ src/Header.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Header.css'
+function Header() {
+  return (
+    <div className="header">
+      <div className="header__left">
+        {/* avatar */}
+        {/* time icon */}
+      </div>
+      
+      <div className="header__search">
+        {/* search icon */}
+        {/* input */}
+      </div>
+
+      <div className="header__right">
+        {/* help icon */}
+      </div>
+    </div>
+  )
+}
+
+export default Header
